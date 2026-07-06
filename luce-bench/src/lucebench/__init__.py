easel/lucebox-hub

feat(luce-bench): standalone harness with multi-turn replay + LLM-judge grading
## What

Adds the luce-bench/ Python package as a standalone bench harness:

- Core areas: smoke, gsm8k, hellaswag, humaneval, truthfulqa_mc1,
  longctx, agent, agent_recorded (multi-turn replay), forge, ds4_eval.
- Multi-turn agent_recorded replay with an LLM-judge grader and
  per-turn metrics; forge_eval fixture imported under
  fixtures/forge_eval/_forge/.
- Card sampling, snapshot/submit_baseline, model-card schema,
  thinking-budget client, normalize+regrade pipeline, hostinfo, and
  CLI entrypoint.
- Fixtures: agent_cases, agent_recorded (single + multi_turn),
  ds4_eval_cases, agent_prompts (codex variants).
- Tests: ~25 pytest modules covering every area plus thinking
  control, normalize/regrade, snapshot, runner, and host-info paths.
- scripts/extract-agentic-fixture.py (loaded by
  test_extract_agentic_fixture.py via path) and the
  scripts/check_card_bundle_drift.sh CI gate.

## Why

Splits the bench harness out of lucebox-hub's main tree so it can
ship as its own installable package and be consumed by lucebox bench
without pulling in the C++ server build context. Multi-turn replay +
LLM-judge grading is what unblocks the coding-agent-loop sweep
workflow.

## Dependencies

None - this PR is independent.

luce-bench/src/lucebench/__init__.py
@@ -1,0 +1,27 @@
+"""luce-bench — capability benchmarks for chat-completion endpoints.
+
+Quick start:
+
+    from lucebench.areas import ds4_eval
+    from lucebench.runner import run_case
+
+    cases = ds4_eval.load_cases()
+    rows = [run_case(url="http://localhost:8080", case=c, model="dflash")
+            for c in cases]
+
+Or via CLI:
+
+    lucebench --url http://localhost:8080 --area ds4-eval --model dflash
+
+The package vendors evaluation fixtures from upstream MIT-licensed
+projects (antirez/ds4, openai/human-eval, antoinezambelli/forge). See
+NOTICE for attribution.
+"""
+
+# Version is generated by hatch-vcs at build time into _version.py.
+# Fresh source-tree checkouts before any build will not yet have the
+# file — fall back to a dev marker so imports don't break.
+try:
+    from lucebench._version import __version__
+except ImportError:
+    __version__ = "0.0.0.dev0+unbuilt"

Luce-Org/lucebox

luce-bench/src/lucebench/__init__.py
@@ -1,0 +1,27 @@
+"""luce-bench — capability benchmarks for chat-completion endpoints.
+
+Quick start:
+
+    from lucebench.areas import ds4_eval
+    from lucebench.runner import run_case
+
+    cases = ds4_eval.load_ds4_eval_cases()
+    rows = [run_case(url="http://localhost:8080", case=c, model="dflash")
+            for c in cases]
+
+Or via CLI:
+
+    lucebench --url http://localhost:8080 --areas ds4-eval --model dflash
+
+The package vendors evaluation fixtures from upstream MIT-licensed
+projects (antirez/ds4, openai/human-eval, antoinezambelli/forge). See
+NOTICE for attribution.
+"""
+
+# Version is generated by hatch-vcs at build time into _version.py.
+# Fresh source-tree checkouts before any build will not yet have the
+# file — fall back to a dev marker so imports don't break.
+try:
+    from lucebench._version import __version__
+except ImportError:
+    __version__ = "0.0.0.dev0+unbuilt"